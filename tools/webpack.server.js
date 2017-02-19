63f4306b9a504b6a020b67cee11f7508a0e87c5c
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2017-02-20 02:35:37 +0800

    add dynamic entry

--- tools/webpack.server.js
+++ tools/webpack.server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var webpack = require('webpack');
+var MultiEntryPlugin = require('webpack/lib/MultiEntryPlugin');
 var webpackDevMiddleware = require("webpack-dev-middleware");
 var webpackHotMiddleware = require("webpack-hot-middleware");
 var proxy = require('proxy-middleware');
+var path = require('path');
 
 var webpackConfig = require("./webpack.dev.js"),
 	config = require("../config/project");
@@ -14,18 +16,60 @@ for (var key in webpackConfig.entry) {
 	webpackConfig.entry[key].unshift('react-hot-loader/patch');
 }
 
-var compiler = webpack(webpackConfig);
-app.use(webpackDevMiddleware(compiler, {
-    hot: true,
-	historyApiFallback: true,
-	noInfo: true,
-	stats: { 
-		colors: true 
-	},
-}));
-app.use(webpackHotMiddleware(compiler));
+var compiler = webpack(webpackConfig),
+	devMiddleWare = webpackDevMiddleware(compiler, {
+	    hot: true,
+		historyApiFallback: true,
+		// noInfo: true,
+		stats: { 
+			chunks: false,
+			colors: true 
+		},
+	}),
+	hotMiddleWare = webpackHotMiddleware(compiler);
+
+
+app.use(devMiddleWare);
+
+app.use(function(req, res, next) {
+
+	let url = req.url;
+
+	let basename = path.basename(url, '.html'),
+		key = 'js/' + basename;
+	
+	if (url.indexOf(config.route) === 0 && config.webpack.entry[key] && !webpackConfig.entry[key]) {
+
+		console.log('entryChunk: ' + key);
+
+		config.webpack.entry[key].unshift('webpack-hot-middleware/client');
+		config.webpack.entry[key].unshift('react-hot-loader/patch');
+
+		compiler.apply(new MultiEntryPlugin(process.cwd(), config.webpack.entry[key], key));
+
+		devMiddleWare.invalidate(); // 强制重新构建一次，不用调用多次，后续的触发由 webpack 自己 hot reload
+		webpackConfig.entry[key] = config.webpack.entry[key];
+		delete config.webpack.entry[key];
+
+		compiler.plugin('done', () => {
+			console.log('webpack-dev-seveer invalidate done!');
+			setTimeout(() => {
+				next();
+			}, 300);
+			
+		});
+
+	}
+	else {
+		next();
+	}
+});
+
+app.use(hotMiddleWare);
+
 // 前端转发
 app.use(config.route, proxy('http://localhost:' + port));
+
 // 后台转发
 app.use('/api/', proxy('http://localhost:3001'));
 
